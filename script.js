4716784264bda5a38b16a6e360cc3a785c6f46ab
Med Ayoub Haj Amor <ayoubhajamor.xc@gmail.com>
2025-12-01 22:21:31 +0100

    hide projects and experiences using js

---
+++ script.js
@@ -0,0 +1,10 @@
+function hide(elm) {
+    elm.setAttribute("hidden", "");
+}
+
+let projects = document.querySelector("#projects");
+let experience = document.querySelector("#experience");
+
+hide(projects);
+hide(experience);
+
