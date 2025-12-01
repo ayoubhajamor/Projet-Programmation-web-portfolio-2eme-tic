2d319b549bd63db7d6610cad68aa50ab348fb2a8
Med Ayoub Haj Amor <ayoubhajamor.xc@gmail.com>
2025-12-02 00:49:26 +0100

    Update hide() to accept many arguments and add display to unhide items

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-function hide(elm) {
-    elm.setAttribute("hidden", "");
+function hide() {
+    for (let i = 0; i < arguments.length; i++)
+        arguments[i].setAttribute("hidden", "");
+}
+function display() {
+    for (let i = 0; i < arguments.length; i++)
+        arguments[i].removeAttribute("hidden", "");
 }
 
 let projects = document.querySelector("#projects");
 let experience = document.querySelector("#experience");
 
-hide(projects);
-hide(experience);
-
+hide(projects, experience);
