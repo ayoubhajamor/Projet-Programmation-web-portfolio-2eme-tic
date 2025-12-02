cba1c1134181e45aa9f69c40885ba4190d4098eb
Med Ayoub Haj Amor <ayoubhajamor.xc@gmail.com>
2025-12-02 01:45:28 +0100

    refactor js and add switching between home, projects and experience

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 function hide() {
     for (let i = 0; i < arguments.length; i++)
-        arguments[i].setAttribute("hidden", "");
+        arguments[i].style.display = "none";
 }
-function display() {
-    for (let i = 0; i < arguments.length; i++)
-        arguments[i].removeAttribute("hidden", "");
+function display(elm, type) {
+    if (type == "home")
+        elm.style.display = home_display_default;
+    if (type == "projects") 
+        elm.style.display = "flex";
+    if (type == "experience")
+        elm.style.display = "flex";
 }
+let home = document.querySelector("#home");
+let home_display_default = "flex";
 
 let projects = document.querySelector("#projects");
+// TODO : fix this
+let projects_display_default = projects.style.display;
+
 let experience = document.querySelector("#experience");
+// TODO : And this
+let experience_display_default = experience.style.display;
+
 
 hide(projects, experience);
+
+
+// Switch between home, experience and projects
+document.querySelector("#switch_home").addEventListener("click", () => {
+    hide(projects, experience);
+    display(home, "home");
+});
+document.querySelector("#switch_experience").addEventListener("click", () => {
+    hide(projects, home);
+    display(experience, "experience");
+});
+
+document.querySelector("#switch_projects").addEventListener("click", () => {
+    hide(experience, home);
+    display(projects, "projects");
+});
+
