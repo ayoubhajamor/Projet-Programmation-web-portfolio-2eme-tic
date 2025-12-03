9e681846a2e85315266d8076e2f7df3338c2fb57
Med Ayoub Haj Amor <ayoubhajamor.xc@gmail.com>
2025-12-03 04:23:37 +0100

    refactor hide() and display() to use classes

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
 function hide() {
     for (let i = 0; i < arguments.length; i++)
-        arguments[i].style.display = "none";
+        arguments[i].classList.add("hidden");
 }
-function display(elm, type) {
-    if (type == "home")
-        elm.style.display = home_display_default;
-    if (type == "projects") 
-        elm.style.display = "flex";
-    if (type == "experience")
-        elm.style.display = "flex";
+function display() {
+    for (let i = 0; i < arguments.length; i++)
+        arguments[i].classList.remove("hidden");
 }
 let home = document.querySelector("#home");
-let home_display_default = "flex";
 
 let projects = document.querySelector("#projects");
-// TODO : fix this
-let projects_display_default = projects.style.display;
 
 let experience = document.querySelector("#experience");
-// TODO : And this
-let experience_display_default = experience.style.display;
 
 
 hide(projects, experience);
@@ -28,15 +19,15 @@ hide(projects, experience);
 // Switch between home, experience and projects
 document.querySelector("#switch_home").addEventListener("click", () => {
     hide(projects, experience);
-    display(home, "home");
+    display(home);
 });
 document.querySelector("#switch_experience").addEventListener("click", () => {
     hide(projects, home);
-    display(experience, "experience");
+    display(experience);
 });
 
 document.querySelector("#switch_projects").addEventListener("click", () => {
     hide(experience, home);
-    display(projects, "projects");
+    display(projects);
 });
 
